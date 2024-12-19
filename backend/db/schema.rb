69752bc76d1183efc79563a6cb49d60affbcb07f
JoseHiro <ipho.7johero@icloud.com>
2024-12-19 12:03:14 -0600

    delete function

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_12_12_040154) do
-  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
